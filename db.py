d81f7473d9f8fe5f49fd9cf7cbed68dd80138c20
GSofiaG <GSofiaGx@gmail.com>
2024-08-30 16:48:59 -0500

    Servidor3

---
+++ db.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+def conectar():
+    conector = sqlite3.connect('productos.db')
+    conector.row_factory = sqlite3.Row
+    return conector
+
+def init_db():
+    conector = conectar()
+    conector.execute(''' 
+        CREATE TABLE IF NOT EXISTS productos(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NUL, 
+            descripcion TEXT,
+            precio REAL NOT NULL
+            imagen TEXT                                                            
+                     )
+    ''')
+    conector.commit() 
+    conector.close()
+
+def get_all_products():
+    conector = conectar()
+    productos = conector.execute('SELECT * FROM productos').fetcha11()
+    conector.close()
+    return productos
+
+def add_product(nombre, descripcion, precio, imagen):
+    conector = conectar()
+    conector.execute('INSERT INTO productos(nombre, descripcion, precio, imagen) VALUES (?,?,?,?)' (nombre, descripcion, precio, imagen))
+    conector.commit()
+    conector.close()
+
+def update_products(id, nombre, descripcion, precio, imagen):
+    conector = conectar()
+    conector.execute('''
+    UPDATE productos
+    SET nombre=?, descripcion=?, precio=?, imagen=?
+    WHERE id=?                                                         
+    ''',(nombre, descripcion, precio, imagen, id))
+    conector.commit()
+    conector.close()
+
+def delete_product(id):
+    conector = conectar()
+    conector.execute('DELETE FROM productos WHERE id=?', (id,))
+    conector.close()
+
+
+
